8efae25fe12e80df2e60ee3829f45b6ce8e78176
Joseph Emelike <juemelike@gmail.com>
2022-09-25 14:56:47 -0400

    Created using Colaboratory

---
+++ recipe_gen/notebooks/example_train.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyP91HxzZZO4QtqXsUCRkCHP",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/jemelike/recipe-personalization/blob/setup_improvements/recipe_gen/notebooks/example_train.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "WKBtTol4AQVK"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
